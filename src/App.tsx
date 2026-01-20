9408a21050511b825a7f8860a29da6633bfe87af
italoat <49888152+italoat@users.noreply.github.com>
2026-01-20 11:04:24 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { MainLayout } from './layouts/MainLayout';
+import { Dashboard } from './pages/Dashboard';
+import { PartsSearch } from './pages/PartsSearch';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="search" element={<PartsSearch />} />
+          
+          {/* Rotas Placeholders para os outros módulos */}
+          <Route path="*" element={<div className="p-10 text-slate-500">Módulo em desenvolvimento...</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
