053efd3c6f424fe6e5f10d3f13a808e3336dd5f4
italoat <49888152+italoat@users.noreply.github.com>
2026-01-20 11:53:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { MainLayout } from './layouts/MainLayout';
 import { Dashboard } from './pages/Dashboard';
 import { PartsSearch } from './pages/PartsSearch';
+import { Login } from './pages/Login';
+
+// Componente que protege a rota
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const user = localStorage.getItem('user');
+  return user ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
+        <Route path="/login" element={<Login />} />
+        
+        {/* Todas as rotas internas protegidas */}
+        <Route path="/" element={
+          <PrivateRoute>
+            <MainLayout />
+          </PrivateRoute>
+        }>
           <Route index element={<Dashboard />} />
           <Route path="search" element={<PartsSearch />} />
           
-          {/* Rotas Placeholders para os outros módulos */}
+          {/* Rotas Placeholder */}
           <Route path="*" element={<div className="p-10 text-slate-500">Módulo em desenvolvimento...</div>} />
         </Route>
       </Routes>
